9b530c8fccb26d166a70427f32ea3c6cac03fbd3
VoVietNhan <vvnhan.work@gmail.com>
2023-12-26 22:56:45 +0700

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
-import './css/font.css'
-import './css/size.css'
-import './css/color.css'
-import SignIn from './page/SignIn/signIn';
-import SignUp from './page/SignUp/signUp';
-import Header from './components/header';
-
+import "./css/font.css";
+import "./css/size.css";
+import "./css/color.css";
+import SignIn from "./page/SignIn/signIn";
+import SignUp from "./page/SignUp/signUp";
+import {Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-        {/* <SignIn /> */}
-        <SignUp />
-        {/* <Header /> */}
+    <Routes>
+      <Route path="/" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
     </div>
   );
 }
