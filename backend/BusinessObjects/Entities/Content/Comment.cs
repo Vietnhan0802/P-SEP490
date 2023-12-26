bfff2c60a02483340a89c7b75a4cfca4b1a90501
MinhThuw <95035935+MinhThuuu@users.noreply.github.com>
2023-12-26 13:42:05 +0700

    entity

---
+++ backend/BusinessObjects/Entities/Content/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Entities.Content
+{
+    public class Comment
+    {
+        public Guid idComment { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public Guid idPost { get; set; }
+        public Post? Post { get; set; }
+        public ICollection<Reply>? Replies { get; set; } = new List<Reply>();
+    }
+}
---
+++ backend/BusinessObjects/Entities/Content/Reply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Entities.Content
+{
+    public class Reply
+    {
+        public Guid idReply { get; set; } 
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public Guid idComment { get; set; } 
+        public Comment? Comment { get; set; }    
+    }
+}
