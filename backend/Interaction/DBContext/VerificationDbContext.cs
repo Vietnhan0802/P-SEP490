db883dddedb76efaf3ae30c2b5b03334e421fdf1
hoangluongthai07@gmail.com <hoangluongthai06@gmail.com>
2023-12-26 23:29:25 +0700

    branch_V2

---
+++ backend/Interaction/DBContext/VerificationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BusinessObjects.Entities.Interaction;
+using Microsoft.EntityFrameworkCore;
+
+namespace Interaction.DBContext
+{
+    public class VerificationDbContext : DbContext
+    {
+        public DbSet<Verification> Verifications { get; set; }
+
+        public VerificationDbContext(DbContextOptions<VerificationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Verification> verifications { get; set; }
+    }
+}
