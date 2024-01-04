6980026346572e4789291ce88d92fd68f580d27a
VoVietNhan <vvnhan.work@gmail.com>
2024-01-04 21:32:04 +0700

    fixx

--- frontend/src/axios/axiosConfig.js
+++ frontend/src/axios/axiosConfig.js
@@ -1,45 +1,38 @@
-// axiosConfig.js
-const axios = require('axios');
+// axiosInstance.js
+import axios from 'axios';
 
-const createAxiosInstance = (baseURL) => {
-  const instance = axios.create({
-    baseURL: baseURL,
-    timeout: 5000,
-    headers: {
-      'Content-Type': 'application/json',
-      // Thêm các headers khác nếu cần thiết
-    },
-  });
+const instance = axios.create({
+  baseURL: 'http://localhost:5195/api/User/', // Set the base URL for the instance
+  timeout: 5000, // Set a timeout for requests (in milliseconds)
+  headers: {
+    'Content-Type': 'application/json',
+    // Add any other headers you need for all requests from this instance
+  },
+});
 
-  // Thêm interceptor cho request
-  instance.interceptors.request.use(
-    config => {
-      console.log(`Sending request to ${config.url}`);
-      // Có thể thêm logic xử lý request ở đây
-      return config;
-    },
-    error => {
-      return Promise.reject(error);
-    }
-  );
+// Add request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    // Do something with the request config, such as adding headers or modifying the data
+    console.log(config.data);
+    return config;
+  },
+  (error) => {
+    // Handle request error
+    return Promise.reject(error);
+  }
+);
 
-  // Thêm interceptor cho response
-  instance.interceptors.response.use(
-    response => {
-      console.log(`Received response from ${response.config.url}`);
-      // Có thể thêm logic xử lý response ở đây
-      return response;
-    },
-    error => {
-      console.error('Error in response:', error);
-      return Promise.reject(error);
-    }
-  );
+// Add response interceptor
+instance.interceptors.response.use(
+  (response) => {
+    // Do something with the response data
+    return response;
+  },
+  (error) => {
+    // Handle response error
+    return Promise.reject(error);
+  }
+);
 
-  return instance;
-};
-
-module.exports = {
-  axiosInstanceApi1: createAxiosInstance('http://localhost:3001'),
-  axiosInstanceApi2: createAxiosInstance('http://localhost:3002'),
-};
+export default instance;
---
+++ frontend/src/page/SignUp/signUp.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logoImg from "../../images/common/logo.png";
+import GGIcon from "../../images/common/gg-icon.png";
+import FBIcon from "../../images/common/fb-icon.png";
+import "../SignUp/signUp.css";
+
+import { useState } from "react";
+import PersonForm from "./PersonForm";
+import BusinessForm from "./BusinessForm";
+
+
+export default function SignIn() {
+  const [showForm1, setShowForm1] = useState(true);
+  const [showForm2, setShowForm2] = useState(false);
+
+  const handleFormClick = (formNumber) => {
+    // Reset the visibility of both forms
+    setShowForm1(false);
+    setShowForm2(false);
+
+    // Set the visibility of the clicked form
+    if (formNumber === 1) {
+      setShowForm1(true);
+    } else if (formNumber === 2) {
+      setShowForm2(true);
+    }
+  };
+  const [inputs, setInputs] = useState({});
+  const handleClickGG = () => {
+    // implementation details
+  };
+  const handleClickFB = () => {
+    // implementation details
+  };
+
+  return (
+    <section
+      id="signInForm"
+      className="signIn-bg hV-100 d-flex align-items-center"
+    >
+      <div className="container">
+        <div className="d-flex form-shadow roundedL-28 roundedR-28 flex-column-reverse flex-lg-row">
+          <div className="col-lg-7 col roundedL-28 white-bg pt-2 pb-4">
+            <p className="SFU-bold size-40 blue2f text-center py-lg-3 py-lg-5">
+              SIGN UP
+            </p>
+            <div className="d-flex justify-content-between">
+            <button onClick={() => handleFormClick(1)} className="gray-border white-bg d-flex flex-row rounded-50 align-items-center justify-content-center">Person</button>
+            <button onClick={() => handleFormClick(2)} className="gray-border white-bg d-flex flex-row rounded-50 align-items-center justify-content-center">Business</button>
+           
+            </div>
+             <div className="form-area">
+              {showForm1 && (
+                <PersonForm />
+              )}
+              {showForm2 && (
+                <BusinessForm />
+
+              )}
+              <div className="row justify-content-center align-items-center pt-2">
+                <div className="col line1"></div>
+                <div className="col-5 col-lg-4 col-md-4 col-sm-4 text-center size-20">
+                  Sign up with
+                </div>
+                <div className="col line1"></div>
+              </div>
+              <div className="d-flex flex-row pt-3 pb-3">
+                <div className="d-flex col-6 google-btn justify-content-end">
+                  <button
+                    className="gray-border white-bg d-flex flex-row rounded-50 align-items-center justify-content-center"
+                    type="button"
+                    onClick={handleClickGG}
+                  >
+                    <img src={GGIcon} alt="GGIcon" />
+                    <p>Google</p>
+                  </button>
+                </div>
+                <div className="d-flex col-6 facebook-btn">
+                  <button
+                    className="gray-border white-bg d-flex flex-row rounded-50 align-items-center justify-content-center"
+                    type="button"
+                    onClick={handleClickFB}
+                  >
+                    <img src={FBIcon} alt="FBIcon" />
+                    <p>Facebook</p>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col roundedR-28 wc-bg SFU-reg white">
+            <div className="d-flex justify-content-center justify-content-lg-start">
+              <img className="pt-4 px-lg-3 pb-lg-5" src={logoImg} alt="logo" />
+            </div>
+            <div className="SFU-bold px-4 pb-3 pt-lg-5 mt-lg-5 text-center text-lg-start ">
+              <p className="size-40 d-lg-none">Welcome Back!</p>
+              <p className="d-none d-lg-block size-70">
+                Welcome&nbsp;
+                <br />
+                Back!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
