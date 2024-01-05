54a428dccd18d6b51ef33ae4da70e68de1e0ef22
VoVietNhan <vvnhan.work@gmail.com>
2024-01-05 16:35:38 +0700

    add react boostrap

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,20 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/header.css";
 export default function Header() {
   return (
-    <section>
-      <header id="header">
-        <div className="logo">
-
-        </div>
-        <div className="bar"></div>
-        <div className="action"></div>
-      </header>
-      <div className="temp">
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-      </div>
-    </section>
+    <header>
+      <div></div>
+      <div></div>
+      <div></div>
+    </header>
   );
 }
