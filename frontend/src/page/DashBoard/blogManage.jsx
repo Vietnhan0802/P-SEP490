e86960806f765bd6cb6b718cbbac367fc5b13c85
poqmmpoq <vantoitran2002@gmail.com>
2024-01-11 01:42:05 +0700

    pushDassh

---
+++ frontend/src/page/DashBoard/blogManage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../scss/dbOptions.scss";
+export default function BlogManage() {
+  return (
+    <div className="optionCard">
+      <div className="size-12">Manage Blog</div>
+      <div className="size-24 SFU-heavy">New Post</div>
+      <div className="size-14 h-100 d-flex justify-content-end blue2f">
+        View Detail
+      </div>
+    </div>
+  );
+}
