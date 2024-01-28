1c921efa063ed21dcf70424fc66da73de5c27f02
Son Trinh <son1932001@gmail.com>
2024-01-28 15:50:34 +0700

    again

--- backend/User/Controllers/UserController.cs
+++ backend/User/Controllers/UserController.cs
@@ -4,8 +4,6 @@ using BusinessObjects.Enums.User;
 using BusinessObjects.ViewModels.User;
 using Commons.Helpers;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
