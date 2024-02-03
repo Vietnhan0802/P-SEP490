cb911da777ca845b752e3119aeef69cd40ed2c1b
Son Trinh <son1932001@gmail.com>
2024-02-03 13:33:23 +0700

    xong blog

---
+++ backend/Blog/Validator/CreateUpdateBlogCommentValidator.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObjects.ViewModels.Blog;
+using FluentValidation;
+
+namespace Blog.Validator
+{
+    public class CreateUpdateBlogCommentValidator : AbstractValidator<CreateUpdateBlogComment>
+    {
+        public CreateUpdateBlogCommentValidator()
+        {
+            RuleFor(x => x.content).NotEmpty().WithMessage("The comment shouldn't empty!")
+                                 .MaximumLength(500).WithMessage("The comment must less than 500 characters!");
+        }
+    }
+}
