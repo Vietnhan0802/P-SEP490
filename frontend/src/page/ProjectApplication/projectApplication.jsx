7237a80e60791aa983f2f5e23730f9747ea14647
poqmmpoq <vantoitran2002@gmail.com>
2024-03-02 16:29:32 +0700

    project application

--- frontend/src/page/ProjectApplication/projectApplication.jsx
+++ frontend/src/page/ProjectApplication/projectApplication.jsx
@@ -1,134 +1,110 @@
 import React from "react";
 import "./projectApplication.scss";
 import avatar from "../../images/common/Avatar.png";
+import Table, { SelectColumnFilter } from "./table";
 
-const requests = [
-  {
-    name: "Olivia Rhye",
-    email: "olivia@untitledui.com",
-    avatar: "path-to-olivia-avatar.jpg",
-    date: "21 Jan 2024",
-    project: "Lack of relevant and substantial information.",
-  },
-  // Thêm các yêu cầu khác tại đây
-];
+const getData = () => {
+  const data = [
+    {
+      name: "Jane Cooper",
+      email: "jane.cooper@example.com",
+      title: "Regional Paradigm Technician",
+      department: "Optimization",
+      status: "Active",
+      role: "Admin",
+      imgUrl:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    },
+    {
+      name: "Cody Fisher",
+      email: "cody.fisher@example.com",
+      title: "Product Directives Officer",
+      department: "Intranet",
+      status: "Active",
+      role: "Owner",
+      imgUrl:
+        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    },
+    {
+      name: "Esther Howard",
+      email: "esther.howard@example.com",
+      title: "Forward Response Developer",
+      department: "Directives",
+      status: "Active",
+      role: "Member",
+      imgUrl:
+        "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    },
+    {
+      name: "Jenny Wilson",
+      email: "jenny.wilson@example.com",
+      title: "Central Security Manager",
+      department: "Program",
+      status: "Active",
+      role: "Member",
+      imgUrl:
+        "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    },
+    {
+      name: "Kristin Watson",
+      email: "kristin.watson@example.com",
+      title: "Lean Implementation Liaison",
+      department: "Mobility",
+      status: "Active",
+      role: "Admin",
+      imgUrl:
+        "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    },
+    {
+      name: "Cameron Williamson",
+      email: "cameron.williamson@example.com",
+      title: "Internal Applications Engineer",
+      department: "Security",
+      status: "Active",
+      role: "Member",
+      imgUrl:
+        "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    },
+  ];
+  return [...data, ...data, ...data, ...data];
+};
 
 function ProjectApplication() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Title",
+        accessor: "title",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+      {
+        Header: "Role",
+        accessor: "role",
+        Filter: SelectColumnFilter, // new
+        filter: "includes", // new
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => getData(), []);
   return (
-    <div id="project_application ">
-      <div className="block p-2 bg-white border-8">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Member</th>
-              <th>Date</th>
-              <th>Project</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {requests.map((request, index) => (
-              <tr key={index}>
-                <td>
-                  <div className="d-flex align-items-center">
-                    <img
-                      src={avatar}
-                      className="rounded-circle me-2"
-                      alt={request.name}
-                      style={{ width: "40px", height: "40px" }}
-                    />
-                    <div>
-                      <h2 className="size-16 SFU-bold mb-0">{request.name}</h2>
-                      <p className="text-gray-600 SFU-reg text-muted">
-                        {request.email}
-                      </p>
-                    </div>
-                  </div>
-                </td>
-                <td>{request.date}</td>
-                <td className="size-16 SFU-bold">{request.project}</td>
-                <td>
-                  <button className="btn btn-outline-success btn-sm me-2">
-                    Accept
-                  </button>
-                  <button className="btn btn-outline-danger btn-sm me-2">
-                    Reject
-                  </button>
-                  <button className="btn btn-outline-primary btn-sm">
-                    View
-                  </button>
-                </td>
-              </tr>
-            ))}
-           {requests.map((request, index) => (
-              <tr key={index}>
-                <td>
-                  <div className="d-flex align-items-center">
-                    <img
-                      src={avatar}
-                      className="rounded-circle me-2"
-                      alt={request.name}
-                      style={{ width: "40px", height: "40px" }}
-                    />
-                    <div>
-                      <h2 className="size-16 SFU-bold mb-0">{request.name}</h2>
-                      <p className="text-gray-600 SFU-reg text-muted">
-                        {request.email}
-                      </p>
-                    </div>
-                  </div>
-                </td>
-                <td>{request.date}</td>
-                <td className="size-16 SFU-bold">{request.project}</td>
-                <td>
-                  <button className="btn btn-outline-success btn-sm me-2">
-                    Accept
-                  </button>
-                  <button className="btn btn-outline-danger btn-sm me-2">
-                    Reject
-                  </button>
-                  <button className="btn btn-outline-primary btn-sm">
-                    View
-                  </button>
-                </td>
-              </tr>
-            ))}
-            {requests.map((request, index) => (
-              <tr key={index}>
-                <td>
-                  <div className="d-flex align-items-center">
-                    <img
-                      src={avatar}
-                      className="rounded-circle me-2"
-                      alt={request.name}
-                      style={{ width: "40px", height: "40px" }}
-                    />
-                    <div>
-                      <h2 className="size-16 SFU-bold mb-0">{request.name}</h2>
-                      <p className="text-gray-600 SFU-reg text-muted">
-                        {request.email}
-                      </p>
-                    </div>
-                  </div>
-                </td>
-                <td>{request.date}</td>
-                <td className="size-16 SFU-bold">{request.project}</td>
-                <td>
-                  <button className="btn btn-outline-success btn-sm me-2">
-                    Accept
-                  </button>
-                  <button className="btn btn-outline-danger btn-sm me-2">
-                    Reject
-                  </button>
-                  <button className="btn btn-outline-primary btn-sm">
-                    View
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+    <div className="min-vh-100 bg-light text-dark">
+      <main className="container py-4">
+        <div className="">
+          <h1 className="h2 fw-semibold">React Table + Bootstrap 5 = ❤</h1>
+        </div>
+        <div className="mt-4">
+          <Table columns={columns} data={data} />
+        </div>
+      </main>
     </div>
   );
 }
