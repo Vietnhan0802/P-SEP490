7bbb913269fe6a8981669ab15d07ffc01a2c566e
VoVietNhan <vvnhan.work@gmail.com>
2024-03-07 14:03:23 +0700

    fix

--- frontend/src/components/follow.jsx
+++ frontend/src/components/follow.jsx
@@ -1,45 +1,41 @@
-import React from "react";
+import React, { useEffect, useState,} from "react";
 import "../scss/follow.scss";
-import avatar from "../images/common/Avatar.png";
 import tick from "../images/common/verifiedTick.png";
 import { LuDot } from "react-icons/lu";
 import { useTranslation } from 'react-i18next';
-function Follow() {
-  const {t} = useTranslation()
+import { followInstance } from "../axios/axiosConfig";
+import defaultImage from "../../src/images/common/default.png";
+import { useNavigate } from "react-router-dom";
+
+function Follow({ newFollow }) {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [following, setFollowing] = useState([]);
+  const sessionData = JSON.parse(sessionStorage.getItem("userSession")) || {};
+  const { currentUserId } = sessionData;
+  useEffect(() => {
+    followInstance.get(`GetAllFollowings/${currentUserId}`)
+      .then((res) => { setFollowing(res?.data?.result) })
+      .catch((error) => { console.error(error) })
+  }, [newFollow])
+  const handleAvatarclick = (value) => {
+    navigate('/profile', { state: { userId: value } });
+  }
   return (
     <div className="follow position-relative">
       <div>
         <p className="text">{t('following')}</p>
-        <div className="follow-user d-flex align-items-center mb-2">
-          <div className="follow-avata-box">
-            <img src={avatar} alt="user" className="user-image" />
-            <img src={tick} alt="tick" className="user-tick" />
-          </div>
-          <div className="follow-user-info ms-2">
-            <p className="follow-user-name fw-bold">Luna Verse</p>
-            <p className="follow-user-email">Lorem ipsum dolor sit amet</p>
-          </div>
-        </div>
-        <div className="follow-user d-flex align-items-center mb-2">
-          <div className="follow-avata-box">
-            <img src={avatar} alt="user" className="user-image" />
-            <img src={tick} alt="tick" className="user-tick" />
-          </div>
-          <div className="follow-user-info ms-2">
-            <p className="follow-user-name fw-bold">Luna Verse</p>
-            <p className="follow-user-email">Lorem ipsum dolor sit amet</p>
-          </div>
-        </div>
-        <div className="follow-user d-flex align-items-center mb-2">
-          <div className="follow-avata-box">
-            <img src={avatar} alt="user" className="user-image" />
-            <img src={tick} alt="tick" className="user-tick" />
-          </div>
-          <div className="follow-user-info ms-2">
-            <p className="follow-user-name fw-bold">Luna Verse</p>
-            <p className="follow-user-email">Lorem ipsum dolor sit amet</p>
-          </div>
-        </div>
+        {following.map((item) => (
+          <div className="follow-user d-flex align-items-center mb-2" key={item.idAccount} onClick={()=>handleAvatarclick(item.idAccount)}>
+            <div className="follow-avata-box">
+              <img src={item.avatar === "https://localhost:7006/Images/" ? defaultImage : item.avatar} alt="user" className="user-image" />
+              <img src={tick} alt="tick" className="user-tick" />
+            </div>
+            <div className="follow-user-info ms-2">
+              <p className="follow-user-name fw-bold">{item.fullName}</p>
+              <p className="follow-user-email">{item.email}</p>
+            </div>
+          </div>))}
       </div>
 
       <button className="btn border mt-3 fw-bold">
